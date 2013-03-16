ecd97c18f4542f232163d1676f827356d4f22508
Jyri-Matti Lähteenmäki <jyri-matti@lahteenmaki.net>
2013-03-16 21:25:33 +0200

    improved and prettified Collections

--- src/main/java/fi/solita/utils/functional/Collections.java
+++ src/main/java/fi/solita/utils/functional/Collections.java
@@ -13,112 +13,73 @@ import java.util.Map;
 import java.util.Set;
 
 public abstract class Collections {
+    
+    public static <T> List<T> emptyList() {
+        return java.util.Collections.emptyList();
+    }
+    
+    public static <T> Set<T> emptySet() {
+        return java.util.Collections.emptySet();
+    }
+    
+    public static <K,V> Map<K,V> emptyMap() {
+        return java.util.Collections.emptyMap();
+    }
 
     public static <T> List<T> newList() {
         return new ArrayList<T>();
     }
     
-    public static List<Boolean> newList(boolean[] array) {
-        Boolean[] result = new Boolean[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = Boolean.valueOf(array[i]);
-        }
-        return Arrays.asList(result);
-    }
-    
-    public static List<Byte> newList(byte[] array) {
-        Byte[] result = new Byte[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = Byte.valueOf(array[i]);
-        }
-        return Arrays.asList(result);
-    }
-    
-    public static List<Character> newList(char[] array) {
-        Character[] result = new Character[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = Character.valueOf(array[i]);
-        }
-        return Arrays.asList(result);
-    }
-    
-    public static List<Double> newList(double[] array) {
-        Double[] result = new Double[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = Double.valueOf(array[i]);
-        }
-        return Arrays.asList(result);
-    }
-    
-    public static List<Float> newList(float[] array) {
-        Float[] result = new Float[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = Float.valueOf(array[i]);
-        }
-        return Arrays.asList(result);
-    }
-
-    public static List<Integer> newList(int[] array) {
-        Integer[] result = new Integer[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = Integer.valueOf(array[i]);
-        }
-        return Arrays.asList(result);
-    }
-    
-    public static List<Long> newList(long[] array) {
-        Long[] result = new Long[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = Long.valueOf(array[i]);
-        }
-        return Arrays.asList(result);
-    }
-    
-    public static List<Short> newList(short[] array) {
-        Short[] result = new Short[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = Short.valueOf(array[i]);
-        }
-        return Arrays.asList(result);
-    }
-
-    public static <T> List<T> newListOfSize(int initialSize) {
-        return new ArrayList<T>(initialSize);
-    }
-
     public static <T> Set<T> newSet() {
         return new HashSet<T>();
+    }
+
+    public static <K, V> Map<K, V> newMap() {
+        return new HashMap<K, V>();
+    }
+    
+    public static <T> List<T> newListOfSize(int initialSize) {
+        return new ArrayList<T>(initialSize);
     }
 
     public static <T> Set<T> newSetOfSize(int initialSize) {
         return new HashSet<T>(initialSize);
     }
 
-    public static <K, V> Map<K, V> newMap() {
-        return new HashMap<K, V>();
-    }
-
     public static <K,V> Map<K,V> newMapOfSize(int initialSize) {
         return new HashMap<K,V>(initialSize);
     }
+    
+    public static List<Boolean> newList(boolean... array) {
+        return newList(newArray(array));
+    }
+    
+    public static List<Byte> newList(byte... array) {
+        return newList(newArray(array));
+    }
+    
+    public static List<Character> newList(char... array) {
+        return newList(newArray(array));
+    }
+    
+    public static List<Double> newList(double... array) {
+        return newList(newArray(array));
+    }
+    
+    public static List<Float> newList(float... array) {
+        return newList(newArray(array));
+    }
 
-    public static <T> List<T> newList(Iterable<T> elements) {
-    	List<T> ret = null;
-    	for (int size: Iterables.resolveSize(elements)) {
-    	    ret = newListOfSize(size);
-    	}
-    	if (ret == null) {
-            ret = newList();
-        }
-
-    	if (elements instanceof Collection) {
-    	    ret.addAll((Collection<? extends T>) elements);
-    	} else {
-        	for (T t: elements) {
-        		ret.add(t);
-        	}
-    	}
-        return java.util.Collections.unmodifiableList(ret);
+    public static List<Integer> newList(int... array) {
+        return newList(newArray(array));
+    }
+    
+    public static List<Long> newList(long... array) {
+        return newList(newArray(array));
+    }
+    
+    public static List<Short> newList(short... array) {
+        return newList(newArray(array));
     }
     
     public static <T> List<T> newList(T[] elements) {
@@ -158,6 +119,96 @@ public abstract class Collections {
     public static <T> List<T> newList(T e1, T e2, T e3, T e4, T e5, T e6, T... elements) {
         return newList(concat(newList(e1, e2, e3, e4, e5, e6), elements));
     }
+    
+    public static Set<Boolean> newSet(boolean... array) {
+        return newSet(newArray(array));
+    }
+    
+    public static Set<Byte> newSet(byte... array) {
+        return newSet(newArray(array));
+    }
+    
+    public static Set<Character> newSet(char... array) {
+        return newSet(newArray(array));
+    }
+    
+    public static Set<Double> newSet(double... array) {
+        return newSet(newArray(array));
+    }
+    
+    public static Set<Float> newSet(float... array) {
+        return newSet(newArray(array));
+    }
+
+    public static Set<Integer> newSet(int... array) {
+        return newSet(newArray(array));
+    }
+    
+    public static Set<Long> newSet(long... array) {
+        return newSet(newArray(array));
+    }
+    
+    public static Set<Short> newSet(short... array) {
+        return newSet(newArray(array));
+    }
+    
+    public static <T> Set<T> newSet(T[] elements) {
+        return newSet(Arrays.asList(elements));
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T> Set<T> newSet(T e1) {
+        return newSet(Arrays.asList(e1));
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T> Set<T> newSet(T e1, T e2) {
+        return newSet(Arrays.asList(e1, e2));
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T> Set<T> newSet(T e1, T e2, T e3) {
+        return newSet(Arrays.asList(e1, e2, e3));
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T> Set<T> newSet(T e1, T e2, T e3, T e4) {
+        return newSet(Arrays.asList(e1, e2, e3, e4));
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T> Set<T> newSet(T e1, T e2, T e3, T e4, T e5) {
+        return newSet(Arrays.asList(e1, e2, e3, e4, e5));
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T> Set<T> newSet(T e1, T e2, T e3, T e4, T e5, T e6) {
+        return newSet(Arrays.asList(e1, e2, e3, e4, e5, e6));
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T> Set<T> newSet(T e1, T e2, T e3, T e4, T e5, T e6, T... elements) {
+        return newSet(concat(Arrays.asList(e1, e2, e3, e4, e5, e6), elements));
+    }
+    
+    public static <T> List<T> newList(Iterable<T> elements) {
+        List<T> ret = null;
+        for (int size: Iterables.resolveSize(elements)) {
+            ret = newListOfSize(size);
+        }
+        if (ret == null) {
+            ret = newList();
+        }
+
+        if (elements instanceof Collection) {
+            ret.addAll((Collection<? extends T>) elements);
+        } else {
+            for (T t: elements) {
+                ret.add(t);
+            }
+        }
+        return java.util.Collections.unmodifiableList(ret);
+    }
 
     public static <T> Set<T> newSet(Iterable<T> elements) {
         Set<T> ret = null;
@@ -177,10 +228,6 @@ public abstract class Collections {
         return java.util.Collections.unmodifiableSet(ret);
     }
 
-    public static <T> Set<T> newSet(T... elements) {
-        return newSet(Arrays.asList(elements));
-    }
-
     public static <K, V> Map<K, V> newMap(Iterable<? extends Map.Entry<? extends K, ? extends V>> elements) {
         Map<K, V> ret = null;
         for (int size: Iterables.resolveSize(elements)) {
@@ -189,10 +236,10 @@ public abstract class Collections {
         if (ret == null) {
             ret = newMap();
         }
-    	for (Map.Entry<? extends K, ? extends V> e: elements) {
-    		ret.put(e.getKey(), e.getValue());
-    	}
-    	return java.util.Collections.unmodifiableMap(ret);
+        for (Map.Entry<? extends K, ? extends V> e: elements) {
+            ret.put(e.getKey(), e.getValue());
+        }
+        return java.util.Collections.unmodifiableMap(ret);
     }
 
     @SuppressWarnings("unchecked")
@@ -208,12 +255,140 @@ public abstract class Collections {
     public static <K, V> Map<K, V> newMap(Map.Entry<? extends K, ? extends V>... elements) {
         return newMap(Arrays.asList(elements));
     }
+    
+    public static Boolean[] newArray(boolean... array) {
+        Boolean[] result = new Boolean[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static Byte[] newArray(byte... array) {
+        Byte[] result = new Byte[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static Character[] newArray(char... array) {
+        Character[] result = new Character[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static Double[] newArray(double... array) {
+        Double[] result = new Double[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static Float[] newArray(float... array) {
+        Float[] result = new Float[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+
+    public static Integer[] newArray(int... array) {
+        Integer[] result = new Integer[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static Long[] newArray(long... array) {
+        Long[] result = new Long[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static Short[] newArray(short... array) {
+        Short[] result = new Short[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static boolean[] newArray(Boolean[] array) {
+        boolean[] result = new boolean[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static byte[] newArray(Byte[] array) {
+        byte[] result = new byte[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static char[] newArray(Character[] array) {
+        char[] result = new char[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static double[] newArray(Double[] array) {
+        double[] result = new double[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static float[] newArray(Float[] array) {
+        float[] result = new float[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+
+    public static int[] newArray(Integer[] array) {
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static long[] newArray(Long[] array) {
+        long[] result = new long[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
+    
+    public static short[] newArray(Short[] array) {
+        short[] result = new short[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+        return result;
+    }
 
     public static <T> T[] newArray(Iterable<T> elements, Class<T> clazz) {
-    	List<T> list = newList(elements);
-    	@SuppressWarnings("unchecked")
-    	T[] ret = (T[])Array.newInstance(clazz, list.size());
-    	return list.toArray(ret);
+        List<T> list = newList(elements);
+        @SuppressWarnings("unchecked")
+        T[] ret = (T[])Array.newInstance(clazz, list.size());
+        return list.toArray(ret);
     }
     
     public static Iterable<Character> it(CharSequence charSeq) {
