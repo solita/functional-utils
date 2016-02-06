500dba751488f5187b1519cc50b4f51cd14c2b22
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-02-06 21:21:49 +0200

    Builder/Lens improvements

--- src/main/java/fi/solita/utils/functional/Lens.java
+++ src/main/java/fi/solita/utils/functional/Lens.java
@@ -1,19 +1,91 @@
 package fi.solita.utils.functional;
 
-import java.util.Map;
+import static fi.solita.utils.functional.Collections.newList;
+import static fi.solita.utils.functional.Collections.newSet;
+import static fi.solita.utils.functional.Functional.map;
+import static fi.solita.utils.functional.Functional.reduce;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 public final class Lens<T,F> {
-    private final Apply<T, F> getter;
-    private final Apply<Map.Entry<? extends T, ? extends F>, T> setter;
+    private final Apply<? super T, F> getter;
+    private final Function2<T, Apply<F,F>, T> setter;
     private final Builder<T> builder;
+    
+    private static final <A,B> Lens<A,B> each(final Lens<A,? extends Iterable<B>> lens, final Monoid<B> g, final Apply<Iterable<B>,? extends Iterable<B>> h) {
+        return new Lens<A,B>(new Apply<A,B>() {
+            public B apply(A a) {
+                return reduce(g, lens.get(a));
+            }
+        }, new Function2<A,Apply<B,B>,A>() {
+            @SuppressWarnings("unchecked")
+            public A apply(A a, Apply<B,B> f) {
+                Iterable<B> bs = lens.getter.apply(a);
+                return lens.builder.init(a)
+                              .with((Apply<A,Iterable<B>>)lens.getter, h.apply(map(f, bs)))
+                              .build();
+            }
+        },
+        lens.builder);
+    }
+    
+    public static final <A,B> Lens<A,B> each(final Lens<A,Iterable<B>> lens, final Monoid<B> g) {
+        return each(lens, g, new Apply<Iterable<B>, Iterable<B>>() {
+            public Iterable<B> apply(Iterable<B> t) {
+                return newList(t);
+            }
+        });
+    }
+    
+    public static final <A,B> Lens<A,B> eachCollection(final Lens<A,Collection<B>> lens, final Monoid<B> g) {
+        return each(lens, g, new Apply<Iterable<B>, Collection<B>>() {
+            public Collection<B> apply(Iterable<B> t) {
+                return newList(t);
+            }
+        });
+    }
+    
+    public static final <A,B> Lens<A,B> eachList(final Lens<A,List<B>> lens, final Monoid<B> g) {
+        return each(lens, g, new Apply<Iterable<B>, List<B>>() {
+            public List<B> apply(Iterable<B> t) {
+                return newList(t);
+            }
+        });
+    }
+    
+    public static final <A,B> Lens<A,B> eachSet(final Lens<A,Set<B>> lens, final Monoid<B> g) {
+        return each(lens, g, new Apply<Iterable<B>, Set<B>>() {
+            public Set<B> apply(Iterable<B> t) {
+                return newSet(t);
+            }
+        });
+    }
+    
+    public static final <A,B> Lens<A,B> option(final Lens<A,Option<B>> lens, final Function0<B> orElse) {
+        return new Lens<A,B>(new Apply<A,B>() {
+            public B apply(A a) {
+                return lens.get(a).cata(Function.<B>id(), orElse);
+            }
+        }, new Function2<A,Apply<B,B>,A>() {
+            public A apply(A a, Apply<B,B> f) {
+                Option<B> bs = lens.getter.apply(a);
+                return lens.builder.init(a)
+                              .with(lens.getter, bs.map(f))
+                              .build();
+            }
+        },
+        lens.builder);
+    }
 
-    private Lens(Apply<T, F> getter, Apply<Map.Entry<? extends T, ? extends F>, T> setter, Builder<T> builder) {
+    private Lens(Apply<? super T, F> getter, Function2<T, Apply<F,F>, T> setter, Builder<T> builder) {
         this.getter = getter;
         this.setter = setter;
         this.builder = builder;
     }
 
-    public static final <T,F> Lens<T,F> of(Apply<T, F> getter, Builder<T> builder) {
+    public static final <T,F> Lens<T,F> of(Apply<? super T, F> getter, Builder<T> builder) {
         return new Lens<T,F>(getter, setter(getter, builder), builder);
     }
 
@@ -22,7 +94,11 @@ public final class Lens<T,F> {
     }
 
     public final T set(T t, F newValue) {
-        return setter.apply(Tuple.of(t, newValue));
+        return setter.apply(t, Function.<F,F>constant(newValue));
+    }
+    
+    public final T over(T t, Apply<F,F> f) {
+        return setter.apply(t, f);
     }
     
     public final <O> Lens<T,O> andThen(final Lens<F,O> otherLens) {
@@ -30,23 +106,24 @@ public final class Lens<T,F> {
             public O apply(T t) {
                 return otherLens.get(getter.apply(t));
             }
-        }, new Function2<T,O,T>() {
-            public T apply(T tt, O otherValue) {
+        }, new Function2<T,Apply<O,O>,T>() {
+            public T apply(T tt, Apply<O,O> f) {
                 F otherObject = otherLens.builder.init(getter.apply(tt)).build();
                 return builder.init(tt)
-                              .with(getter, otherLens.set(otherObject, otherValue))
+                              .with(getter, otherLens.over(otherObject, f))
                               .build();
             }
         },
         builder);
     }
 
-    private static final <T,F> Apply<Map.Entry<? extends T,? extends F>,T> setter(final Apply<T,F> field, final Builder<T> builder) {
-        return new Apply<Map.Entry<? extends T,? extends F>, T>() {
-            public T apply(Map.Entry<? extends T, ? extends F> t) {
-                return builder.init(t.getKey())
-                              .with(field, t.getValue())
-                              .build();
+    private static final <T,F> Function2<T,Apply<F,F>,T> setter(final Apply<? super T,F> field, final Builder<T> builder) {
+        return new Function2<T,Apply<F,F>, T>() {
+            @Override
+            public T apply(T t1, Apply<F, F> t2) {
+                return builder.init(t1)
+                        .with(field, t2.apply(field.apply(t1)))
+                        .build();
             }
         };
     }
