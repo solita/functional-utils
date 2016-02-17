d92fabf1bc3591308564dcf66fd5837911e422fe
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-02-17 10:44:14 +0200

    I guess Tuple.of(a,b) could return a Pair...

--- src/main/java/fi/solita/utils/functional/Tuple.java
+++ src/main/java/fi/solita/utils/functional/Tuple.java
@@ -164,7 +164,7 @@ public abstract class Tuple implements Serializable {
     }
     
     public static <T1, T2> Tuple2<T1, T2> of(T1 _1, T2 _2) {
-        return new Tuple2<T1, T2>(_1, _2);
+        return new Pair<T1, T2>(_1, _2);
     }
     
     public static <T1, T2, T3> Tuple3<T1, T2, T3> of(T1 _1, T2 _2, T3 _3) {
