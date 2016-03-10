e6376138b6b62cefee69c10d5ed5313b2ae159aa
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-03-10 22:45:32 +0200

    added some pattern matching

--- src/main/java/fi/solita/utils/functional/Match.java
+++ src/main/java/fi/solita/utils/functional/Match.java
@@ -2,11 +2,21 @@ package fi.solita.utils.functional;
 
 import static fi.solita.utils.functional.Collections.newArray;
 import static fi.solita.utils.functional.Collections.newList;
+import static fi.solita.utils.functional.Collections.newListOfSize;
+import static fi.solita.utils.functional.FunctionalA.max;
 import static fi.solita.utils.functional.FunctionalA.zip;
+import static fi.solita.utils.functional.FunctionalC.drop;
+import static fi.solita.utils.functional.FunctionalC.isEmpty;
+import static fi.solita.utils.functional.FunctionalC.span;
+import static fi.solita.utils.functional.FunctionalC.take;
 import static fi.solita.utils.functional.Option.None;
 import static fi.solita.utils.functional.Option.Some;
+import static fi.solita.utils.functional.Predicates.equalTo;
+import static fi.solita.utils.functional.Predicates.not;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class Match {
     @SuppressWarnings("unchecked")
@@ -28,6 +38,91 @@ public abstract class Match {
         return None();
     }
     
+    public static final Option<Pair<String,String>> startsWith(String prefix, CharSequence xs) {
+        int prefixLength = prefix.length();
+        CharSequence left = take(prefixLength, xs).toString();
+        if (left.equals(prefix)) {
+            return Some(Pair.of(prefix, drop(prefixLength, xs).toString()));
+        }
+        return Option.None();
+    }
+    
+    public static final Option<Pair<String,String>> endsWith(String suffix, CharSequence xs) {
+        int suffixLength = suffix.length();
+        int prefixLength = xs.length() - suffixLength;
+        CharSequence right = drop(prefixLength, xs).toString();
+        if (right.equals(suffix)) {
+            return Some(Pair.of(take(prefixLength, xs).toString(), suffix));
+        }
+        return Option.None();
+    }
+    
+    public static final Option<Tuple2<String,String>> tuple2(char separator, CharSequence xs) {
+        List<String> parts = split(separator, xs);
+        if (parts.size() == 2) {
+            return Some(Tuple.of(parts.get(0), parts.get(1)));
+        }
+        return None();
+    }
+    
+    public static final Option<Pair<String,String>> pair(char separator, CharSequence xs) {
+        List<String> parts = split(separator, xs);
+        if (parts.size() == 2) {
+            return Some(Pair.of(parts.get(0), parts.get(1)));
+        }
+        return None();
+    }
+    
+    public static final Option<Tuple3<String,String,String>> tuple3(char separator, CharSequence xs) {
+        List<String> parts = split(separator, xs);
+        if (parts.size() != 3) {
+            return Some(Pair.of(parts.get(0), parts.get(1), parts.get(2)));
+        }
+        return None();
+    }
+    
+    public static final Option<Tuple4<String,String,String,String>> tuple4(char separator, CharSequence xs) {
+        List<String> parts = split(separator, xs);
+        if (parts.size() != 4) {
+            return Some(Pair.of(parts.get(0), parts.get(1), parts.get(2), parts.get(3)));
+        }
+        return None();
+    }
+    
+    public static final Option<Tuple5<String,String,String,String,String>> tuple5(char separator, CharSequence xs) {
+        List<String> parts = split(separator, xs);
+        if (parts.size() != 5) {
+            return Some(Pair.of(parts.get(0), parts.get(1), parts.get(2), parts.get(3), parts.get(4)));
+        }
+        return None();
+    }
+
+    private static final List<String> split(char separator, CharSequence xs) {
+        Predicate<Character> predicate = not(equalTo(separator));
+        List<String> parts = newList();
+        
+        boolean cont = !isEmpty(xs);
+        while (cont) {
+            Pair<CharSequence,CharSequence> p = span(predicate, xs);
+            parts.add(p.left.toString());
+            xs = drop(1, p.right);
+            cont = !isEmpty(xs) || !isEmpty(p.right);
+        }
+        return parts;
+    }
+    
+    public static final Option<List<String>> groups(Pattern regex, CharSequence xs) {
+        Matcher matcher = regex.matcher(xs);
+        if (matcher.matches()) {
+            List<String> groups = newListOfSize(max(0, matcher.groupCount() - 1));
+            for (int i = 1; i <= matcher.groupCount(); ++i) {
+                groups.add(matcher.group(i));
+            }
+            return Some(groups);
+        }
+        return None();
+    }
+    
     public static final <T> Option<T> singleton(Iterable<T> ts) {
         List<T> arr = newList(ts);
         if (arr.size() == 1) {
@@ -36,10 +131,42 @@ public abstract class Match {
         return None();
     }
     
+    public static final <T> Option<Tuple2<T,T>> tuple2(Iterable<T> ts) {
+        List<T> arr = newList(ts);
+        if (arr.size() == 2) {
+            return Some(Tuple.of(arr.get(0), arr.get(1)));
+        }
+        return None();
+    }
+    
     public static final <T> Option<Pair<T,T>> pair(Iterable<T> ts) {
         List<T> arr = newList(ts);
         if (arr.size() == 2) {
             return Some(Pair.of(arr.get(0), arr.get(1)));
+        }
+        return None();
+    }
+    
+    public static final <T> Option<Tuple3<T,T,T>> tuple3(Iterable<T> ts) {
+        List<T> arr = newList(ts);
+        if (arr.size() == 3) {
+            return Some(Pair.of(arr.get(0), arr.get(1), arr.get(2)));
+        }
+        return None();
+    }
+    
+    public static final <T> Option<Tuple4<T,T,T,T>> tuple4(Iterable<T> ts) {
+        List<T> arr = newList(ts);
+        if (arr.size() == 4) {
+            return Some(Pair.of(arr.get(0), arr.get(1), arr.get(2), arr.get(3)));
+        }
+        return None();
+    }
+    
+    public static final <T> Option<Tuple5<T,T,T,T,T>> tuple5(Iterable<T> ts) {
+        List<T> arr = newList(ts);
+        if (arr.size() == 5) {
+            return Some(Pair.of(arr.get(0), arr.get(1), arr.get(2), arr.get(3), arr.get(4)));
         }
         return None();
     }
--- src/test/java/fi/solita/utils/functional/MatchTest.java
+++ src/test/java/fi/solita/utils/functional/MatchTest.java
@@ -1,11 +1,14 @@
 package fi.solita.utils.functional;
 
+import static fi.solita.utils.functional.Collections.emptyList;
 import static fi.solita.utils.functional.Collections.newList;
 import static fi.solita.utils.functional.Functional.sequence;
+import static fi.solita.utils.functional.Option.None;
 import static fi.solita.utils.functional.Option.Some;
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
@@ -90,5 +93,49 @@ public class MatchTest {
          }
          
          assertEquals(newList('a', 'b', 'c', 'd'), matching);
+    }
+    
+    @Test
+    public void startsWith() {
+        assertEquals(Some(Pair.of("foo", "bar")), Match.startsWith("foo", "foobar"));
+        assertEquals(Some(Pair.of("", "")),       Match.startsWith("", ""));
+        assertEquals(Some(Pair.of("", "bar")),    Match.startsWith("", "bar"));
+        assertEquals(Some(Pair.of("foo", "")),    Match.startsWith("foo", "foo"));
+        
+        assertEquals(None(), Match.startsWith("foo", "bar"));
+        assertEquals(None(), Match.startsWith("foo", ""));
+        assertEquals(None(), Match.startsWith("foobar", "foo"));
+    }
+    
+    @Test
+    public void endsWith() {
+        assertEquals(Some(Pair.of("foo", "bar")), Match.endsWith("bar", "foobar"));
+        assertEquals(Some(Pair.of("", "")),       Match.endsWith("", ""));
+        assertEquals(Some(Pair.of("bar", "")),    Match.endsWith("", "bar"));
+        assertEquals(Some(Pair.of("", "foo")),    Match.endsWith("foo", "foo"));
+        
+        assertEquals(None(), Match.endsWith("foo", "bar"));
+        assertEquals(None(), Match.endsWith("foo", ""));
+        assertEquals(None(), Match.endsWith("foobar", "foo"));
+    }
+    
+    @Test
+    public void stringPair() {
+        assertEquals(Some(Pair.of("a","b")), Match.pair('_', "a_b"));
+        assertEquals(Some(Pair.of("","b")),  Match.pair('_', "_b"));
+        assertEquals(Some(Pair.of("a","")),  Match.pair('_', "a_"));
+        assertEquals(Some(Pair.of("","")), Match.pair('_', "_"));
+        
+        assertEquals(None(), Match.pair('_', "foo"));
+        assertEquals(None(), Match.pair('_', ""));
+    }
+    
+    @Test
+    public void groups() {
+        assertEquals(Some(newList("1","2")), Match.groups(Pattern.compile("(.*)_(.*)"), "1_2"));
+        assertEquals(Some(newList("foo")), Match.groups(Pattern.compile("(.*)"), "foo"));
+        
+        assertEquals(Some(emptyList()), Match.groups(Pattern.compile(".*"), "foo"));
+        assertEquals(None(), Match.groups(Pattern.compile("f(oo)"), "bar"));
     }
 }
