2455df10720c99f7d65f11570aa4654564f280ff
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2021-12-27 09:40:51 +0200

    Either3

---
+++ src/main/java/fi/solita/utils/functional/Either3.java
@@ -0,0 +1,119 @@
+package fi.solita.utils.functional;
+
+import static fi.solita.utils.functional.Collections.newList;
+import static fi.solita.utils.functional.Functional.concat;
+import static fi.solita.utils.functional.Option.Some;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * L or M or R
+ */
+public class Either3<L,M,R> implements Serializable {
+
+    /**
+     * @return {@code value} wrapped into left of Either3.
+     */
+    public static final <L,M,R> Either3<L,M,R> left(L value) {
+        return new Either3<L, M, R>(Some(value), Option.<M>None(), Option.<R>None());
+    }
+    
+    /**
+     * @return {@code value} wrapped into middle of Either3.
+     */
+    public static final <L,M,R> Either3<L,M,R> middle(M value) {
+        return new Either3<L, M, R>(Option.<L>None(), Some(value), Option.<R>None());
+    }
+
+    /**
+     * @return {@code value} wrapped into right of Either3.
+     */
+    public static final <L,M,R> Either3<L,M,R> right(R value) {
+        return new Either3<L, M, R>(Option.<L>None(), Option.<M>None(), Some(value));
+    }
+    
+    public static final <T> T get(Either3<? extends T, ? extends T, ? extends T> e) {
+        return e.isLeft()   ? e.left.get() :
+               e.isMiddle() ? e.middle.get() :
+                              e.right.get();
+    }
+    
+    /**
+     * @return value of either3 in a list.
+     */
+    public static final <T> List<T> asList(Either3<? extends T, ? extends T, ? extends T> either) {
+        return newList(concat(either.left, either.middle, either.right));
+    }
+
+    protected Either3(Option<L> left, Option<M> middle, Option<R> right) {
+        this.left = left;
+        this.middle = middle;
+        this.right = right;
+    }
+
+    /**
+     * Left side.
+     */
+    public final Option<L> left;
+    
+    /**
+     * Middle.
+     */
+    public final Option<M> middle;
+    
+    /**
+     * Right side.
+     */
+    public final Option<R> right;
+    
+    /**
+     * @return whether this Either3 has a Left element.
+     */
+    public final boolean isLeft() {
+        return left.isDefined();
+    }
+    
+    /**
+     * @return whether this Either3 has a Middle element.
+     */
+    public final boolean isMiddle() {
+        return middle.isDefined();
+    }
+    
+    /**
+     * @return whether this Either3 has a Right element.
+     */
+    public final boolean isRight() {
+        return right.isDefined();
+    }
+    
+    /**
+     * @return left transformed with {@code fLeft}, middle transformed with {@code fMiddle} or right transformed with {@code fRight}.
+     */
+    public <T> T fold(Apply<? super L, ? extends T> ifLeft, Apply<? super M, ? extends T> ifMiddle, Apply<? super R, ? extends T> ifRight){
+        return isLeft()   ? ifLeft.apply(left.get()) :
+               isMiddle() ? ifMiddle.apply(middle.get()) :
+                            ifRight.apply(right.get());
+    }
+    
+    @Override
+    public int hashCode() {
+        return 31 + left.hashCode() + middle.hashCode() + right.hashCode();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        return this == obj ||
+               obj instanceof Either3 && left.equals(((Either3<?,?,?>)obj).left)
+                                      && middle.equals(((Either3<?,?,?>)obj).middle)
+                                      && right.equals(((Either3<?,?,?>)obj).right);
+    }
+    
+    @Override
+    public String toString() {
+        return isLeft()   ? "Either3.left(" + left + ")" :
+               isMiddle() ? "Either3.middle(" + middle + ")" :
+                            "Either3.right(" + right + ")";
+    }
+}
